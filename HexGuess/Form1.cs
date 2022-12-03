effdd7171085157f9c55ba9c674dc934c55e638c
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-12-03 18:54:46 -0500

    Changing logic to randomly generate Hex and build RGB from it

--- HexGuess/Form1.cs
+++ HexGuess/Form1.cs
@@ -9,34 +9,46 @@ namespace HexGuess {
         }
 
         private void startGame() {
-            String num1 = randomRGB();
-            String num2 = randomRGB();
-            String num3 = randomRGB();
+            String hexVal1 = createHex();
+            String hexVal2 = createHex();
+            String hexVal3 = createHex();
+            String rgbVal1 = createRgbFromHex(hexVal1);
+            String rgbVal2 = createRgbFromHex(hexVal2);
+            String rgbVal3 = createRgbFromHex(hexVal3);
 
-            rgb1.Text = num1;
-            rgb2.Text = num2;   
-            rgb3.Text = num3;
+            hex1.Text = hexVal1;
+            hex2.Text = hexVal2;
+            hex3.Text = hexVal3;
+            rgb1.Text = rgbVal1;
+            rgb2.Text = rgbVal2;
+            rgb3.Text = rgbVal3;
         }
 
-        private int randomNumber() {
+
+        private String createRgbFromHex(String hex) {
+            String val1 = hex.Substring(0,2);
+            String val2 = hex.Substring(2,2);
+            String val3 = hex.Substring(4,2);
+
+            int rgb1 = Convert.ToInt32(val1, 16);
+            int rgb2 = Convert.ToInt32(val2, 16);
+            int rgb3 = Convert.ToInt32(val3, 16);
+
+            return String.Format("{0:000}", rgb1) + ", "
+                + String.Format("{0:000}", rgb2) + ", "
+                + String.Format("{0:000}", rgb3);
+        }
+
+        private String createHex() {
             Random rnd = new Random();
-            return rnd.Next(0, 255);
-        }
+            String test = "";
+            char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
-        private String randomRGB() {
-            return String.Format("{0:000}", randomNumber()) + ", "
-                +  String.Format("{0:000}", randomNumber()) + ", "
-                +  String.Format("{0:000}", randomNumber());
-        }
+            for (int i = 0; i < 6; i++) {
+                test += hex[rnd.Next(0, 15)];
+            }
 
-
-
-        private void createHexFromRGB() {
-            String rgb = randomRGB();
-
-            char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-            char[] code;
-
+            return test;
         }
     }
 }
